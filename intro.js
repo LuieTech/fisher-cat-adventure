3f4bae36eeb084478902f3103372668e4ba971d8
Luis Ramirez <rhluisenrique@gmail.com>
2023-05-04 16:10:34 -0400

    Initial files

---
+++ intro.js
@@ -0,0 +1,4 @@
+const canvas1 = document.getElementById('canvas1');
+const ctx = canvas1.getContext('2d');
+const canvas2 = document.getElementById('canvas2')
+const ctx2 = canvas2.getContext('2d') 
